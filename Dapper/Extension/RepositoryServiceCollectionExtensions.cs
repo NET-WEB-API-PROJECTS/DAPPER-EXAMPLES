87cf0692e73e20c5d891854e423978e2db7b8c69
DHEERAJ CHINTHALA <dheerajchinthaladevgpinfo@gmail.com>
2025-03-01 18:57:09 +0530

    Add project files.

---
+++ Dapper/Extension/RepositoryServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+
+namespace Dapper.Extension
+{
+    public static class RepositoryServiceCollectionExtensions
+    {
+            #region Public Routines
+            public static void AddRepositories(this IServiceCollection services, Assembly assembly, string connectionString)
+            {
+                // Get all types that are classes and end with "Repository" (this is an example pattern)
+                var repositoryAssembly = Assembly.Load("DALL");
+                var repositoryTypes = repositoryAssembly.GetTypes()
+                .Where(t => t.IsClass && t.Name.EndsWith("Repository"));
+
+                foreach (var repositoryType in repositoryTypes)
+                {
+                    // Assuming the repository has a constructor that takes a connection string
+                    var interfaces = repositoryType.GetInterfaces();
+                    if (interfaces.Any())
+                    {
+                        // Register each repository as Scoped with its interface
+                        foreach (var interfaceType in interfaces)
+                        {
+                            services.AddScoped(interfaceType, provider =>
+                                ActivatorUtilities.CreateInstance(provider, repositoryType, connectionString));
+                        }
+                    }
+                    else
+                    {
+                        // If no interfaces, register the repository directly as Scoped
+                        services.AddScoped(repositoryType, provider =>
+                            ActivatorUtilities.CreateInstance(provider, repositoryType, connectionString));
+                    }
+                }
+            }
+            #endregion
+        
+    }
+}
---
+++ Dapper/Program.cs
@@ -0,0 +1,42 @@
+
+using Dapper.Extension;
+using System.Reflection;
+
+namespace Dapper
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            // Register services with the connection string
+            var connectionString = builder.Configuration.GetConnectionString("conn");
+            // Register services with the connection string
+            builder.Services.AddRepositories(Assembly.GetExecutingAssembly(), connectionString);
+            var app = builder.Build();
+   
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
